1a4bc7b8f562f763e740b9fc257e6bcd71703eba
ALhanoofe <ehanoof@outlook.com>
2025-11-09 20:58:30 +0300

    done with nav ejs

---
+++ server.js
@@ -0,0 +1,33 @@
+//dotenv means to hide sensitive info like passwords
+
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const postControllers = require('./controllers/post.js');
+
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.once("connected", () => {
+  console.log("connected to mongo");
+})
+
+//extended mean we receive same data
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+app.use('/assets', express.static(__dirname + '/assets'));
+
+
+
+app.get('/', async (req, res) => {
+  res.render('index.ejs');
+});
+
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+
+})
